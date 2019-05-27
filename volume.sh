46f2f716a3ca9dc96ed21fcad970b49a661fc034
Roman <velvet-shell@yandex.ru>
2019-05-27 15:44:48 +0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Dependencies: alsa-utils
 
@@ -22,40 +22,41 @@ function is_mic_mute {
 
 function send_notification {
     volume=`get_volume`
-    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
-    dunstify -t 5000 -r 2593 -u normal "  $bar"
+    dunstify -t 2000 -r 2593 -u normal "Volume: $volume"
 }
 
 case $1 in
     up)
         # Set the volume on (if it was muted)
-        amixer -D pulse set Master on > /dev/null
+        amixer set Master on > /dev/null
         # Raise the volume (+ 5%)
-        amixer -D pulse sset Master 5%+ > /dev/null
+        amixer set Master 1%+ > /dev/null
         send_notification
         ;;
     down)
-        amixer -D pulse set Master on > /dev/null
+        amixer set Master on > /dev/null
         # Lower the volume (- 5%)
-        amixer -D pulse sset Master 5%- > /dev/null
+        amixer set Master 1%- > /dev/null
         send_notification
         ;;
     mute)
-    	# Toggle mute
-        amixer -D pulse set Master 1+ toggle > /dev/null
-        if is_vol_mute ; then
-            dunstify -t 5000 -r 2593 -u normal "Audio Muted"
+        # Toggle mute
+        amixer set Master 1+ toggle > /dev/null
+        if is_vol_mute
+        then
+            dunstify -t 2000 -r 2593 -u normal "Audio Muted"
         else
             send_notification
         fi
         ;;
     mic)
-    	# Toggle mute
-        amixer -D pulse set Capture 1+ toggle > /dev/null
-        if is_mic_mute ; then
-            dunstify -t 5000 -r 2593 -u normal "Mic Muted"
+        # Toggle mute
+        amixer set Capture 1+ toggle > /dev/null
+        if is_mic_mute
+        then
+            dunstify -t 2000 -r 2592 -u normal "Mic Muted"
         else
-            dunstify -t 5000 -r 2593 -u normal "Mic Unmuted"
+            dunstify -t 2000 -r 2592 -u normal "Mic Unmuted"
         fi
         ;;
 esac
